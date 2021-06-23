d6239a83b73757e847574771e5e1049c6fee1720
Denis Feliciano <denisf.salles@gmail.com>
2021-06-23 15:01:03 -0300

    add home page to main (test)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/login/LoginPage.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
