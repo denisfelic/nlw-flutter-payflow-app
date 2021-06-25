db3b1333e7218a3f0f51afc07f708702df15e6eb
Denis Feliciano <denisf.salles@gmail.com>
2021-06-25 16:00:52 -0300

    add firebase initialization to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,61 @@
+//await Firebase.initializeApp();
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/app_widget.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
-import 'package:payflow/shared/themes/appcolors.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppFirebase());
 
-class MyApp extends StatelessWidget {
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({Key? key}) : super(key: key);
+
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: HomePage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return _getErrorPage();
+        }
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        }
+        // Otherwise, show something whilst waiting for initialization to complete
+        return _getLoadingScreen();
+      },
+    );
+  }
+
+  Material _getLoadingScreen() {
+    return Material(
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Material _getErrorPage() {
+    return Material(
+      child: Center(
+        child: Text(
+          "Não foi possível iniciar o firebase.",
+          textDirection: TextDirection.ltr,
+        ),
+      ),
     );
   }
 }
