8b0eaaa5a11cb9eb7f3843a864138dc9ea37c782
Denis Feliciano <denisf.salles@gmail.com>
2021-06-26 17:16:30 -0300

    add AppColors to the app theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 
 import 'modules/login/login_page.dart';
 import 'shared/themes/appcolors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        primarySwatch: Colors.orange,
+      ),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/login": (context) => LoginPage(),
         "/home": (context) => HomePage(),
+        "/barcode": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage(),
       },
     );
   }
